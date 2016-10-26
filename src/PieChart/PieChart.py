4baaf2156a4ed22b8ebdab1ac9c68cdfc1c358dc
Louis Bursey <burseylj@mcmaster.ca>
2016-10-25 21:27:05 -0400

    Added random colors to PieChart slices

--- src/PieChart/PieChart.py
+++ src/PieChart/PieChart.py
@@ -1,30 +1,37 @@
 from Tkinter import *
 from Pie import *
+import random
 
 
 class PieChart:
 
         def __init__(self, data=None):
                 self.data = clean_data(data) if data is not None else None
-                
-                
+                      
                 self.master = Tk()
                 self.master.resizable(0,0)
                 self.master.title("PieGraph")  #eventually set by user
 
                 self.pie = get_pie(self.master)
                 self.canvas_size = int(self.pie.cget("height"))
-                self.current_angle = 0                  #will go around the pie, making slices
+                self.current_percent = 0      #will go around the pie, making slices
 
 
-        def getSlice(self, angle, filler='white'):
+        def getSlice(self, percent, last=False):
+                #get a random three bit hexadecimal number.
+                #n/4096 chance of printing same number, shouldn't be a problem.
+                color = "#%03x" % random.randint(0, 0xFFF)
+                if last == True:
+                        percent = 100 - self.current_percent
+                
                 self.pie.create_arc(self.canvas_size/4, self.canvas_size/4,
                                     self.canvas_size*3/4, self.canvas_size*3/4,
                                     outline='black', 
-                                    start=self.current_angle,
-                                    extent=angle,
-                                    style=PIESLICE)
-                self.current_angle += angle
+                                    start=self.current_percent*360,
+                                    extent=percent*360,
+                                    style=PIESLICE,
+                                    fill=color)
+                self.current_percent += percent
 
 
 
@@ -33,6 +40,7 @@ class PieChart:
 
 if __name__ == '__main__':
         PieChart = PieChart()
-        PieChart.getSlice(90)
-        PieChart.getSlice(45)
+        PieChart.getSlice(0.10)
+        PieChart.getSlice(0.60)
+        PieChart.getSlice(0.305, last=True)
         mainloop()
