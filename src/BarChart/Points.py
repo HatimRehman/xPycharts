f672a7d0558f418aa8c56dcee3664ef2d05e4ee1
Sarthak Desai <Sarthak Desai>
2016-11-13 11:27:38 -0500

    minor change to MG, Points updated

--- src/BarChart/Points.py
+++ src/BarChart/Points.py
@@ -1,3 +1,6 @@
+## This function breaks apart the user input and assigns the coordinate pairs to a dictionary (data structure).
+#If there are inconsistencies in the user input (for example a missing y value) the function prints and exception. message
+#@param data is the list of co-ordinate pairs inputted by the user.
 def clean_data(data):
 	
 	try:
@@ -17,8 +20,12 @@ def clean_data(data):
 	return mylist
 
 		
+## This function checks if the data input type is correct(i.e. integers or floats).
+#If data type is not correct function outputs an exception message.
+#@param x is a list of x values from all the co-ordinate pairs.
+#@param y is a list of y values from all the co-ordinate pairs.
 
-def checktype(x, y):
+def checktype(x,y):
 	for value in x:
 		if type (value) != int and type (value) != float:
 			raise Exception("Invalid input types in x")
