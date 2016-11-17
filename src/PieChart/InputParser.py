b22e40873f08a447d4b488ee97528297e1fcdd36
Louis Bursey <burseylj@mcmaster.ca>
2016-11-16 21:47:01 -0500

    fixes to user defined colors in PieChart

--- src/PieChart/InputParser.py
+++ src/PieChart/InputParser.py
@@ -1,34 +1,35 @@
-def clean_data(data):
-	
-	try:
-		name=[ x[0] for x in data  ]
-		number=[ y[1] for y in data  ]
-	except:
-		raise Exception("Inconsistent data")
-		return
-	total =  float(sum(number))
+def clean_data(data, colors):
+
+        try:
+                name=[ x[0] for x in data  ]
+                number=[ y[1] for y in data  ]
+        except:
+                raise Exception("Inconsistent data")
+                return
+        total =  float(sum(number))
         
-	mylist=[]
+        mylist=[]
 
-	checktype(name, number)
-	
-	for i in range (len(x)):
-		d = {"name": name[i], "fraction": number[i]/total}
-		mylist.append(d)
-	print mylist
-	return mylist
 
-		
+        checktype(name, number)
 
-def checktype(x, y, data):
-	for value in name:
-		if type (value) != str:
-			raise Exception("Invalid input types in x")
-	for value in number:
-		if type (value) != int and type (value) != float:
-			raise Exception("Invalid input types in y")
-	checkT=True
-	return
+        for i in range (len(name)):
+                d = {"name": name[i], "fraction": number[i]/total}
+                mylist.append(d)
+        print mylist
+        return mylist
+
+                
+
+def checktype(name, number):
+        for value in name:
+                if type (value) != str:
+                        raise Exception("Invalid input types in x")
+        for value in number:
+                if type (value) != int and type (value) != float:
+                        raise Exception("Invalid input types in y")
+        checkT=True
+        return
 
 
 
--- src/PieChart/PieChart.py
+++ src/PieChart/PieChart.py
@@ -1,12 +1,13 @@
 from Tkinter import *
 from Pie import *
+from InputParser import *
 import random
 
 
 class PieChart:
 
-        def __init__(self, data=None):
-                self.data = clean_data(data) if data is not None else None
+        def __init__(self, data, colors=list()):
+                self.data = clean_data(data, colors) if data is not None else None
                       
                 self.master = Tk()
                 self.master.resizable(0,0)
@@ -16,30 +17,44 @@ class PieChart:
                 self.canvas_size = int(self.pie.cget("height"))
                 self.current_percent = 0      #will go around the pie, making slices
 
+                slices = len(self.data)
+                while(len(colors) < slices):
+                        colors.append('yellow')
 
-        def getSlice(self, percent, color=None):
+                self._build_chart(self.data, colors)
+
+        def _get_slice(self, percent, color):
                 #get a random three bit hexadecimal number.
                 #n/4096 chance of printing same number, shouldn't be a problem.
-                if color == None:
-                        color = self.genColor()
-                
+                start=self.current_percent*360
+                extent=percent*360
+                if( 359 < extent + start < 360): extent = 360 - start
                 self.pie.create_arc(self.canvas_size/4, self.canvas_size/4,
                                     self.canvas_size*3/4, self.canvas_size*3/4,
                                     outline='black', 
-                                    start=self.current_percent*360,
+                                    start=start,
                                     extent=percent*360,
                                     style=PIESLICE,
                                     fill=color)
                 self.current_percent += percent
-
-        def genColor(self):
+                return
+                
+        #get a random three bit hexadecimal number.
+        #n/4096 chance of printing same number, shouldn't be a problem.
+        def _gen_color(self):
                 color = "#%03x" % random.randint(0, 0xFFF)
                 return color
-
-
+        
+        def _build_chart(self, data, colors):
+                for i in range(len(data)):
+                        self._get_slice(data[i]['fraction'], colors[i])
+                return
+                        
+                        
+        
 if __name__ == '__main__':
-        PieChart = PieChart()
-        PieChart.getSlice(0.10)
-        PieChart.getSlice(0.60)
-        PieChart.getSlice(0.30)
+
+        colors = ['red', 'white', 'blue']
+        data = [("five percent", 5) , ("ten percent", 10), ("85 percent", 10)]
+        PieChart(data, colors)
         mainloop()
