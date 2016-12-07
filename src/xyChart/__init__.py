67eefcdccd3d9a4c67ecd8ccaaf2623798a297ca
Sarthak Desai <Sarthak Desai>
2016-12-07 17:59:04 -0500

    fixed up the comments for the code

--- src/xyChart/__init__.py
+++ src/xyChart/__init__.py
@@ -1,11 +1,3 @@
-##
-# get_scale\n
-# Author: Louis Bursey\n
-# Function to get the appropriate max and mins for the scale of the graph\n
-# Use round_to to create padding for the graph, ie if the graph goes in increments of 5,
-# set round_to to 5 to keep the graph neat.
-# Returns a tuple in the format [x,y]
-#  @param data_set, the set of data being graphed, in a (x,y) dictionary
-#  @param round_to, an optional parameter to make the scale a multiple of round_to
+# python requires this file to find the directory
 def __init__():
 	return
